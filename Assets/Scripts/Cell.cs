e5659d4f85d410e1bac08340825263d17135afba
O2xygenXD <62666760+O2xygenXD@users.noreply.github.com>
2020-04-05 17:56:18 +0400

    Add files via upload

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    Material oldMat;
+    public Material newMat;
+    Vector3 c_midOfTop;
+    Collider c_collider;
+    Figure figure;
+    
+
+    void Start()
+    {
+        oldMat = GetComponent<Renderer>().material;
+    }
+    void Update()
+    {
+       
+    }
+    public void OnMouseEnter()
+    {
+        setNewColor();
+    }
+    public void OnMouseExit()
+    {
+        setOldColor();
+    }
+    public void setNewColor()
+    {
+        GetComponent<Renderer>().material = newMat;
+    }
+    void setOldColor()
+    {
+        GetComponent<Renderer>().material = oldMat;
+    }
+}
